1683be4fb7ee22baa8c76b9b44a1f36329c2c3e8
Katie <k4lim@ucsd.edu>
2020-02-21 12:55:59 -0800

    Initial commit

---
+++ app/src/main/java/com/example/kathe/quizletjr/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.kathe.quizletjr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
