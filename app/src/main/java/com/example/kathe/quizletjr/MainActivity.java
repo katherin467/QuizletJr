c6307119e977576a731414c7a332b78fff955703
Katie <k4lim@ucsd.edu>
2020-03-27 15:09:00 -0700

    lab 4 complete

--- app/src/main/java/com/example/kathe/quizletjr/MainActivity.java
+++ app/src/main/java/com/example/kathe/quizletjr/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.kathe.quizletjr;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import java.util.List;
@@ -31,8 +35,29 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-               findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+               View questionSideView = findViewById(R.id.flashcard_question);
+               View answerSideView = findViewById(R.id.flashcard_answer);
+
+               questionSideView.setVisibility(View.INVISIBLE);
+               answerSideView.setVisibility(View.VISIBLE);
+
+               // get the center for the clipping circle
+               int cx = answerSideView.getWidth() / 2;
+               int cy = answerSideView.getHeight() / 2;
+
+               // get the final radius for the clipping circle
+               float finalRadius = (float) Math.hypot(cx, cy);
+
+               // create the animator for this view (the start radius is zero)
+               Animator anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy,
+                                                                       0f, finalRadius);
+
+               // hide the question and show the answer to prepare for playing the animation!
+               questionSideView.setVisibility(View.INVISIBLE);
+               answerSideView.setVisibility(View.VISIBLE);
+
+               anim.setDuration(1500);
+               anim.start();
             }
         });
 
@@ -41,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
             }
         });
 
@@ -65,18 +91,44 @@ public class MainActivity extends AppCompatActivity {
                 // advance our pointer index so we can show the next card
                 currentCardDisplayedIndex++;
 
-                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                // make sure we don't get an IndexOutOfBoundsError if we are on the last index
                 if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
                     currentCardDisplayedIndex = 0;
                 }
 
                 // Only do stuff if there are cards in the database
                 if (allFlashcards.size() > 0) {
-                    // set the question and answer TextViews with data from the database
-                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
-                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
-                    findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
-                    findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                    final Animation leftOutAnim = AnimationUtils.loadAnimation(v.getContext(),
+                            R.anim.left_out);
+                    final Animation rightInAnim = AnimationUtils.loadAnimation(v.getContext(),
+                            R.anim.right_in);
+                    if (findViewById(R.id.flashcard_question).getVisibility() == View.VISIBLE)
+                        findViewById(R.id.flashcard_question).startAnimation(leftOutAnim);
+                    else
+                        findViewById(R.id.flashcard_answer).startAnimation(leftOutAnim);
+
+                    leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+                            // this method is called when the animation first starts
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            // set the question and answer TextViews with data from the database
+                            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                            findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                            findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                            findViewById(R.id.flashcard_question).startAnimation(rightInAnim);
+                        }
+
+                        // don't worry about this
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {}
+                    });
+
+
                 }
             }
         });
